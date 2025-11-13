c8accc3b764b7872840e2214dce7289ec594413a
Rasif <rasifrazak123@gmail.com>
2025-11-14 02:24:37 +0530

    configurations made and intial tasks done

---
+++ app/core/config.py
@@ -0,0 +1,34 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+
+    # App config
+    APP_NAME: str = "APIs for questionnaire chatbot using pdf"
+    DEBUG: bool = True
+    
+    # DB Config
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    DATABASE_URL: str
+
+    # jwt config
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE: int = 30
+    
+    # Redis config
+    REDIS_HOST:str
+    REDIS_PORT:int
+    
+    # LLM config
+    MAX_FILE_SIZE: int = 10 * 1024 * 1024
+    LLM_PROVIDER: str
+    LLM_MODEL: str
+    MAX_TOKEN: int
+    LLM_API_KEY: str
+    TEMPERATURE:float
+    
+settings = Settings()
